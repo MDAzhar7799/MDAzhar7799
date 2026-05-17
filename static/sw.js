e29ff8069f142cf7218a78e118bcc98b1170915a
MD AZHAR <mdazhark735@gmail.com>
2026-05-18 00:37:32 +0530

    feat: turn application into an installable progressive web app (PWA)

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'foodexpress-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
